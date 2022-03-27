2e5d03dc2519c1504abee106c84b6ae0758b751a
Jan-Marten de Boer <janmarten@elgentos.nl>
2022-03-27 16:14:26 +0200

    Add second review

--- pages/reviews.php
+++ pages/reviews.php
@@ -60,6 +60,57 @@ return render(
                     balans is, en om weer geïnspireerd te raken door haar.
                     PARAGRAPH
                 ]
+            ],
+            [
+                'summary' => <<<SUMMARY
+                Ik liep altijd hard, had altijd verschillende dingen tegelijk te
+                doen en kwam altijd tijd tekort
+                SUMMARY,
+                'author' => 'Marit',
+                'reviewBody' => [
+                    <<<PARAGRAPH
+                    Ik liep altijd hard, had altijd verschillende dingen tegelijk
+                    te doen en kwam altijd tijd tekort. Tot ik echt struikelde
+                    en met een gebroken enkel kwam te zitten.
+                    PARAGRAPH,
+                    <<<PARAGRAPH
+                    Het effect van de verplichte rust had nare gevolgen. In
+                    plaats van ontspannen de boel over te laten aan anderen,
+                    werd ik nerveus en trillerig. Ik zocht blindelings naar hulp,
+                    maar iedereen zei hetzelfde: "rustig aan."
+                    PARAGRAPH,
+                    <<<PARAGRAPH
+                    Toen kwam er een advertentie van Anja voorbij. Ze kwam
+                    rustig en wijs op me over, en dus belde ik. Ze reageerde
+                    net zoals ze overkwam. Nuchter en meevoelend. "Lukt het
+                    om naar mijn praktijk te komen, of wil je liever dat ik naar
+                    jou toe kom?"
+                    PARAGRAPH,
+                    <<<PARAGRAPH
+                    Ze is de eerste keer naar mij toe gekomen en heeft me laten
+                    praten. Terwijl ik sprak, pakte ze me bij de hand en ik
+                    voelde warme reiki door mijn arm naar me toe stromen.
+                    Zelf sprak ze net iets langzamer dan normaal en dat werkte
+                    aanstekelijk. Mijn woorden gingen minder struikelen. Mijn
+                    tranen, daarentegen, begonnen te stromen. "Goed zo,
+                    laat maar gaan" zei ze, en legde haar handen op mijn hoofd. Dat was
+                    een doorbraak.
+                    PARAGRAPH,
+                    <<<PARAGRAPH
+                    Ik besefte dat het erg lang geleden was dat ik huilde.
+                    We hebben daarna een serie van 8 behandelingen afgesproken.
+                    Langzaamaan werd mij duidelijk dat ik vol zat met energie en
+                    emoties, die niet van mezelf waren, maar overgenomen, als
+                    kind in het gezin waar ik opgroeide.
+                    Dit heeft ze samen met mij kunnen oplossen en dat maakt dat
+                    ik nu een stuk stabieler in het leven sta, en minder gehaast
+                    ben.
+                    PARAGRAPH,
+                    <<<PARAGRAPH
+                    Ik hoef niet meer alles voor iedereen op te lossen.
+                    Ik ben zo dankbaar voor deze verandering in mijn leven.
+                    PARAGRAPH
+                ]
             ]
         ]
     ]
